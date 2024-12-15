bf5ea2534c415ea6a88db50af225eb1374c3282e
Rishu Kumar <rishukumar.prince@gmail.com>
2024-12-15 12:21:24 +0530

    update the featured_posts schema & sonar properties

--- db/migration/000030_featured_posts.up.sql
+++ db/migration/000030_featured_posts.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE featured_posts (
     feature_post_id SERIAL PRIMARY KEY,
     post_id INT NOT NULL,
-    user_id VARCHAR NOT NULL,
+    user_id VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
     FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
