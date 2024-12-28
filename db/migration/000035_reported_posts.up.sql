84e045d5089094797dd11d8ebae04d231c4fdd9a
Rishu Kumar <rishukumar.prince@gmail.com>
2024-12-28 20:22:06 +0530

    add sonar qube warnings fix

--- db/migration/000035_reported_posts.up.sql
+++ db/migration/000035_reported_posts.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE reported_posts (
     report_id SERIAL PRIMARY KEY,
     post_id INT NOT NULL,
-    reported_by VARCHAR NOT NULL,
+    reported_by VARCHAR(255) NOT NULL,
     reason_id INT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
     FOREIGN KEY (post_id) REFERENCES posts(post_id),
