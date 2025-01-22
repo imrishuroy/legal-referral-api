d4466fabf8fb0d3dfbe61c82f2d694aef1941386
Rishu Kumar <rishukumar.prince@gmail.com>
2025-01-23 01:15:35 +0530

    refactor

--- db/redis/redis.go
+++ db/redis/redis.go
@@ -1,26 +1,27 @@
 package redis
 
-import (
-	"context"
-	"github.com/redis/go-redis/v9"
-	"time"
-)
-
-func NewRedis() *redis.Client {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:         "127.0.0.1:6379",
-		Password:     "",
-		DB:           0,
-		DialTimeout:  10 * time.Second,
-		ReadTimeout:  30 * time.Second,
-		WriteTimeout: 30 * time.Second,
-		PoolSize:     10,
-		PoolTimeout:  30 * time.Second,
-	})
-
-	if err := rdb.Ping(context.Background()).Err(); err != nil {
-		panic(err)
-	}
-
-	return rdb
-}
+//
+//import (
+//	"context"
+//	"github.com/redis/go-redis/v9"
+//	"time"
+//)
+//
+//func NewRedis() *redis.Client {
+//	rdb := redis.NewClient(&redis.Options{
+//		Addr:         "127.0.0.1:6379",
+//		Password:     "",
+//		DB:           0,
+//		DialTimeout:  10 * time.Second,
+//		ReadTimeout:  30 * time.Second,
+//		WriteTimeout: 30 * time.Second,
+//		PoolSize:     10,
+//		PoolTimeout:  30 * time.Second,
+//	})
+//
+//	if err := rdb.Ping(context.Background()).Err(); err != nil {
+//		panic(err)
+//	}
+//
+//	return rdb
+//}
